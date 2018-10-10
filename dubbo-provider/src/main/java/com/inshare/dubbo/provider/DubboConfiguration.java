96126ddbc1b5ae6e76c2fec8df3263ff960446a7
liuchenglong <liuchenglong>
2018-10-10 15:47:56 +0800

    使用 java config 的方式配置 dubbo

---
+++ dubbo-provider/src/main/java/com/inshare/dubbo/provider/DubboConfiguration.java
@@ -0,0 +1,38 @@
+package com.inshare.dubbo.provider;
+
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ProtocolConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 重点 11 使用 java config 的方式配置 dubbo
+ *
+ * @Configuration 文件必须和 springboot 启动 application.java 在一个目录下
+ * @author Chengloong
+ */
+@Configuration
+public class DubboConfiguration {
+    @Bean
+    public ApplicationConfig applicationConfig() {
+        ApplicationConfig applicationConfig = new ApplicationConfig();
+        applicationConfig.setName("dubbo-provider");
+        return applicationConfig;
+    }
+
+    @Bean
+    public RegistryConfig registryConfig() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("zookeeper://localhost:2181");
+        return registryConfig;
+    }
+
+    @Bean
+    public ProtocolConfig protocolConfig(){
+        ProtocolConfig protocolConfig = new ProtocolConfig();
+        protocolConfig.setPort(20880);
+        protocolConfig.setName("dubbo");
+        return protocolConfig;
+    }
+}
--- dubbo-provider/src/main/java/com/inshare/dubbo/provider/DubboProviderApplication.java
+++ dubbo-provider/src/main/java/com/inshare/dubbo/provider/DubboProviderApplication.java
@@ -1,16 +1,19 @@
 package com.inshare.dubbo.provider;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 生产者启动类
- *
+ * 重点 11 使用 java config 的方式配置 dubbo
+ * 在使用 java config 的方式配置时 不能使用 EnableDubboConfiguration 注解
+ * 使用 DubboComponentScan 指定 serverImpl 包路对 @Service 进行扫描
  * @author Chengloong
  */
 @SpringBootApplication
-@EnableDubboConfiguration
+//@EnableDubboConfiguration
+@DubboComponentScan(basePackages = "com.inshare.dubbo.provider.service.impl")
 public class DubboProviderApplication {
 
     public static void main(String[] args) {
